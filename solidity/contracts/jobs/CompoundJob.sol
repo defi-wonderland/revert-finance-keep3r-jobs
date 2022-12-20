7060624b081e92caebe7a271a021d1ccaf5db9b7
Ashitaka <ashitaka@defi.sucks>
2022-12-20 16:52:40 +0100

    feat: project structure

--- solidity/contracts/jobs/CompoundJob.sol
+++ solidity/contracts/jobs/CompoundJob.sol
@@ -4,9 +4,6 @@ pragma solidity >=0.8.4 <0.9.0;
 import '@interfaces/jobs/ICompoundJob.sol';
 import '@contracts/jobs/Keep3rJob.sol';
 
-/**
-  @notice Collects the fees from a list of positions
- */
 contract CompoundJob is ICompoundJob, Keep3rJob {
   constructor(address _governance) payable Governable(_governance) {}
 }
